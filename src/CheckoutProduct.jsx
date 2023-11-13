f20c8f03bd7df229f4a20cda4e85c17499e233d5
Shubham Adelkar <adelkarshubham47@gmail.com>
2023-11-13 18:59:41 +0530

    some changes

--- src/CheckoutProduct.jsx
+++ src/CheckoutProduct.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import './CheckoutProduct.css';
 import StarIcon from '@mui/icons-material/Star';
+import { useStateValue } from './StateProvider';
 
 const CheckoutProduct = ({ id, title, seller, image, price, list, rating, save }) => {
+    const [{ basket }, dispatch] = useStateValue();
+    const removeFromBasket = () => {
+        dispatch({
+            type: 'REMOVE_FROM_BASKET',
+            id: id,
+        })
+    }
     return (
         <div className='checkoutProduct'>
             <img className='checkoutProduct__image' src={image} alt="img" />
@@ -19,7 +27,7 @@ const CheckoutProduct = ({ id, title, seller, image, price, list, rating, save }
                         <p><StarIcon /></p>
                     ))}
                 </div>
-                <button>Remove Item</button>
+                <button onClick={removeFromBasket}>Remove Item</button>
             </div>
         </div>
     )
