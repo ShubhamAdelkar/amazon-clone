fefb352cb14a600a4923d8d327c42a6d435a1a17
Shubham Adelkar <adelkarshubham47@gmail.com>
2023-11-13 20:27:29 +0530

    log in js file

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Login.css'
+
+const Login = () => {
+    return (
+        <div className='login'>
+            <img src='https://pngimg.com/uploads/amazon/amazon_PNG11.png' alt='logo' />
+            
+        </div>
+    )
+}
+
+export default Login
