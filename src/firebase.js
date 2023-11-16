e86928390e97a74b029483793faa721a87cde556
Shubham Adelkar <adelkarshubham47@gmail.com>
2023-11-16 09:49:41 +0530

    firebase stuff

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,8 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyCvFKliXfkpMpss0pHQMWt39gw_Fq4xr-g",
     authDomain: "clone-2aa99.firebaseapp.com",
@@ -8,3 +12,10 @@ const firebaseConfig = {
     appId: "1:116597015336:web:a9a5ea427211f0efdc861d",
     measurementId: "G-5S3QS3700S"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
