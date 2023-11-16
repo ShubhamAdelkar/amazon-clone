fbf135dddd4a6b129fa45be72fc37adbfecfb9ef
Shubham Adelkar <adelkarshubham47@gmail.com>
2023-11-16 10:56:40 +0530

    user stuffs

--- src/App.js
+++ src/App.js
@@ -5,10 +5,32 @@ import Home from './Home';
 import Login from './Login';
 import Checkout from './Checkout';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { auth } from './firebase';
+import { useEffect } from 'react';
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{ }, dispatch] = useStateValue();
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>>', authUser);
+
+      if (authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
+
+
   return (
-    // BEM
     <Router>
       <div className="app">
 
